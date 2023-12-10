ddc4f90bc1884fa699b076796d76c2a7d890f921
HAIDER <aaa5540264@gmail.com>
2023-12-10 17:44:00 +0500

    SCRUM-27 Editing Main.java and module-info.java SCRUM-28

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,28 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/screens/sidebar/SideBar.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		
+		primaryStage.setTitle("POS Impulse");
+		primaryStage.centerOnScreen();
+		primaryStage.setResizable(false);
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,9 @@
 module POSImpulse {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	
+	exports application.screens.sidebar;
+	exports application.screens.dashboard;
 }
