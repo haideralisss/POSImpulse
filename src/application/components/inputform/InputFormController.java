7f3d7998d7eec75d04a183296d837cb4a5260d0b
iabdulwali <abdulwali.nadeem21@outlook.com>
2023-12-16 19:29:14 +0500

    SCRUM-39 Displayed the attributes in terms of fields and labels. (AW)

--- src/application/components/inputform/InputFormController.java
+++ src/application/components/inputform/InputFormController.java
@@ -2,10 +2,15 @@ package application.components.inputform;
 import java.io.IOException;
 import java.util.List;
 
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
 import application.components.datagrid.Attribute;
 import application.components.datagrid.DataGridController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,7 +20,6 @@ import javafx.scene.layout.VBox;
 
 public class InputFormController {
 	
-	@SuppressWarnings("exports")
 	@FXML
 	public Label title;
 	
@@ -23,25 +27,21 @@ public class InputFormController {
 	@FXML
 	public FlowPane inputFlowPane;
 	
-	@SuppressWarnings("exports")
 	@FXML
 	public ImageView titleIcon;
 	
-	@SuppressWarnings("exports")
 	@FXML
 	public VBox anchorVBox, flowVBox;
 	
-	@SuppressWarnings("exports")
 	@FXML
 	public AnchorPane inputAnchorPane, anchorPane;
 	
-	@SuppressWarnings("exports")
 	@FXML
 	public Label heading;
 	
 	List<Attribute> attributes;
 	
-	public void SetupInputForm(String title, List<Attribute> attributes, @SuppressWarnings("exports") AnchorPane anchorPane)
+	public void SetupInputForm(String title, List<Attribute> attributes, AnchorPane anchorPane)
 	{
 		this.title.setText(title);
 		this.anchorPane = anchorPane;
@@ -49,6 +49,60 @@ public class InputFormController {
 		titleIcon.setImage(new Image(getClass().getResource("/assets/" + title.toLowerCase() + "Icon.png").toExternalForm()));
 		titleIcon.setFitWidth(30);
 		titleIcon.setFitHeight(30);
+		//anchorVBox.setVgrow(inputAnchorPane, javafx.scene.layout.Priority.ALWAYS);
+		//inputAnchorPane.setTopAnchor(heading, 10.0);
+		
+		for(Attribute attribute : attributes) {
+			if(attribute.isInput())
+			{
+				if(attribute.getType() == "text")
+				{
+					VBox vBox = new VBox();
+					Label label = new Label(attribute.getAttribute());
+					label.getStyleClass().add("inputLabel");
+					vBox.getChildren().add(label);
+					JFXTextField textField = new JFXTextField();
+					textField.getStyleClass().add("inputField");
+					textField.setPromptText(attribute.getAttribute());
+					vBox.getChildren().add(textField);
+					inputFlowPane.getChildren().add(vBox);	
+				}
+				else if(attribute.getType() == "checkbox")
+				{
+					FlowPane fPane = new FlowPane();
+					Label label = new Label(attribute.getAttribute());
+					label.getStyleClass().add("checkLabel");
+					fPane.getChildren().add(label);
+					fPane.getChildren().add(new JFXCheckBox());
+					fPane.getStyleClass().add("fPaneSpacing");
+					inputFlowPane.getChildren().add(fPane);
+				}
+				else if(attribute.getType() == "password")
+				{
+					VBox vBox = new VBox();
+					Label label = new Label(attribute.getAttribute());
+					label.getStyleClass().add("inputLabel");
+					vBox.getChildren().add(label);
+					JFXPasswordField passwordField = new JFXPasswordField();
+					passwordField.getStyleClass().add("inputField");
+					passwordField.setPromptText(attribute.getAttribute());
+					vBox.getChildren().add(passwordField);
+					inputFlowPane.getChildren().add(vBox);	
+				}
+				else if(attribute.getType() == "date")
+				{
+					VBox vBox = new VBox();
+					Label label = new Label(attribute.getAttribute());
+					label.getStyleClass().add("inputLabel");
+					vBox.getChildren().add(label);
+					DatePicker datePicker = new DatePicker();
+					datePicker.getStyleClass().add("inputField");
+					datePicker.setPromptText(attribute.getAttribute());
+					vBox.getChildren().add(datePicker);
+					inputFlowPane.getChildren().add(vBox);	
+				}
+			}
+		}
 	}
 	
 	public void CancelEvent()
